daa1e62befea4f3fa575d347e3578f930a0fc027
KEERTHANA I <keerthanakeekee025@gmail.com>
2026-09-20 15:05:50 +0530

    connect backend to Railway MySQL

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "keerthanavarun",
-    database: "task_management_db"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
